aeab3d8c1967eaf06b1e0841b504255332234a2f
Stas Polos <polos_ss@groupbwt.com>
2024-07-11 03:19:21 +0300

    feat: add settings

---
+++ src/settings.py
@@ -0,0 +1,36 @@
+# mypy: disable-error-code="call-arg"
+"""Settings for `service-cpv-prediction-enricher` project."""
+
+from functools import lru_cache
+
+from dotenv import find_dotenv
+from pydantic import Extra, Field
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Container with settings from virtual environment.
+
+    Attributes:
+        LOG_LEVEL: Logging level.
+
+    """
+
+    LOG_LEVEL: str = Field(..., env="LOG_LEVEL")
+
+    class Config:
+        """Configure for this container.
+
+        Attributes:
+            env_file (str): Path to env file.
+
+        """
+
+        env_file = find_dotenv(".env")
+        extra = Extra.allow
+
+
+@lru_cache
+def project_settings():
+    """Returns instance Settings."""
+    return Settings()
